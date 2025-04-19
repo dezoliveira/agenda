f4d686117dd04ac07747ae730bb14e2b7f1f3c45
Andres Oliveira <andresoliveira@protonmail.com>
2025-04-19 10:03:05 -0300

    instalação e configuração do firebase + teste

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
+'use client'
+
 import Image from "next/image";
+import { auth } from "./lib/firebaseAuth";
+import { useEffect } from "react";
 
 export default function Home() {
+  useEffect(() => {
+    console.log(auth)
+  })
+
   return (
-    <div>
+    <div className="container">
       <h1>Hello World</h1>
     </div>
   );
