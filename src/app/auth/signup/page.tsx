4e25f044a7f6fffa9b577e701120b5580ff86622
Andres Oliveira <andresoliveira@protonmail.com>
2025-04-20 16:08:46 -0300

    Função de cadastro de usuário

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,19 +1,47 @@
 'use client'
-
-import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { useState, FormEvent } from 'react'
 import styles from '../../login.module.css'
 import Link from 'next/link'
+import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
+import { auth } from '@/app/lib/firebaseAuth'
 
 export default function SignUp() {
+  const router = useRouter()
+
   const [name, setName] = useState<string>("")
   const [email, setEmail] = useState<string>("")
   const [password, setPassword] = useState<string>("")
   const [confirmPassword, setConfirmPassword] = useState<string>("")
   const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState("")
+
+  const handleRegister = async(e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    setError("")
+    setLoading(true)
+
+    try {
+      const userCredentials = await createUserWithEmailAndPassword(auth, email, password)
+      const user = userCredentials.user
+
+      await updateProfile(user, {
+        displayName: name
+      })
+
+      console.log("Usuário registrado", userCredentials.user)
+      setLoading(false)
+      router.push("/")
+      
+    } catch (error: any) {
+      setError(error.message)
+      setLoading(false)
+    }
+  }
 
   return (
     <div className={styles.loginContainer}>
-      <form className={styles.loginContent}>
+      <form className={styles.loginContent} onSubmit={handleRegister}>
         <h1 className={styles.title}>Registrar</h1>
         <div className={styles.inputBox}>
           <label>Nome</label>
