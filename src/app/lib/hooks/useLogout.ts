e849e788f172a7155cefc7f1db50bd5eda8f928c
Andres Oliveira <andresoliveira@protonmail.com>
2025-04-21 17:12:42 -0300

    use Logout hook

---
+++ src/app/lib/hooks/useLogout.ts
@@ -0,0 +1,35 @@
+import { signOut } from "firebase/auth"
+import { useState } from "react"
+import { auth } from "../firebaseAuth"
+
+type UseLogoutResult = {
+  logout: () => Promise<boolean>
+  loading: boolean
+  error: string
+}
+
+export function useLogout(): UseLogoutResult {
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>("")
+
+  const logout = async(): Promise<boolean> => {
+    setLoading(true)
+    setError("")
+
+    try {
+      await signOut(auth)
+      setLoading(false)
+
+      return true
+      
+    } catch (err: any) {
+      setError(err.message || "Erro ao fazer logout")
+      setLoading(false)
+
+      return false
+    }
+  }
+
+  return { logout, loading, error }
+
+}
