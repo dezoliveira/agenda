23bef9a2ec33bdd62e5a151af6bbc0faeed3afae
Andres Oliveira <andresoliveira@protonmail.com>
2025-04-21 02:12:47 -0300

    Botão, estilo e funcionalidade de logar com o Google

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { useRouter } from 'next/navigation'
 import { useLogin } from './lib/hooks/useLogin'
 import styles from '../app/login.module.css'
 import Link from 'next/link'
+import { useGoogleLogin } from './lib/hooks/useGoogleLogin'
+import { FcGoogle } from 'react-icons/fc'
 
 export default function Home() {
   const router = useRouter()
@@ -13,6 +15,7 @@ export default function Home() {
   const [password, setPassword] = useState<string>("")
 
   const { login, error, loading } = useLogin()
+  const { loginWithGoogle, loading: loadingGoogle, error: errorGoogle } = useGoogleLogin()
 
   const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -23,6 +26,14 @@ export default function Home() {
     }
     
     const success = await login(credentials)
+
+    if (success) {
+      router.push("/dashboard")
+    }
+  }
+
+  const handleGoogleLogin = async() => {
+    const success = await loginWithGoogle()
 
     if (success) {
       router.push("/dashboard")
@@ -61,6 +72,24 @@ export default function Home() {
           </button>
         </div>
 
+        <div className={styles.dividerBox}>
+          <span className={styles.divider}></span>
+            <p>OU</p>
+          <span className={styles.divider}></span>
+        </div>
+
+        <div className={styles.inputBox}>
+          <button
+            type="button"
+            className={styles.googleButton}
+            disabled={loadingGoogle}
+            onClick={handleGoogleLogin}
+          >
+            <FcGoogle size={32}/>
+            {loadingGoogle ? "Carregando..." : "Entrar com Google"}
+          </button>
+        </div>
+
         <div className={styles.inputBox}>
           <p>Não possui uma conta ?
             <Link href="/auth/signup">
@@ -70,10 +99,10 @@ export default function Home() {
         </div>
 
         {
-          error && (
+          error || errorGoogle && (
             <>
               <div className={styles.errorMessage}>
-                <p>{error}</p>
+                <p>{error || errorGoogle}</p>
               </div>
             </>
           )
