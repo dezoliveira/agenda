fa9e8d357dc1167c3f368ddcffab3ca8769832a3
Andres Oliveira <andresoliveira@protonmail.com>
2025-04-23 16:08:35 -0300

    adicionado firebase Admin

---
+++ src/app/lib/firebaseAdmin.ts
@@ -0,0 +1,15 @@
+import * as admin from "firebase-admin" 
+
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT_KEY || "{}"
+)
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
+export async function verifyIdToken(token: string) {
+  return await admin.auth().verifyIdToken(token)
+}
